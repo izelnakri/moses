211f7ddd4ef978944b529a81894dffc990fb0619
Izel Nakri <contact@izelnakri.com>
2015-09-20 20:46:42 +0200

    initial html structure

---
+++ moses.js
@@ -0,0 +1,40 @@
+//add here the code parser script tag
+// then select every tag and put them in the area
+$(document).ready(function() {
+  var createScriptTag = function () {
+    return $('<script/>', {
+      type: "syntaxhighlighter",
+      class: "brush: xml"
+    });
+  }
+
+  var filteredDom = $("#styleguide").children()
+                .not("h1").not("h2").not("p")
+                .not("script").not("style").not("link");
+
+
+  var getWrapper = function (html) {
+    html = html.wrap("<div class='target'></div>");
+
+    return createScriptTag().html("<![CDATA[ " + html.parent().html() + " ]]>");
+  }
+
+  var createHtmlView = function (domElement) {
+    domElement.append(getWrapper(domElement));
+  }
+
+  filteredDom.each(function(i, element) {
+    createHtmlView($(element));
+  });
+
+  SyntaxHighlighter.all()      
+
+  // function removeToolbars () {
+  //   $(".toolbar").each(function(i, e) { 
+  //     $(e).hide(); 
+  //   });
+  // }
+
+  // window.setTimeout(removeToolbars, 2000);
+
+});
