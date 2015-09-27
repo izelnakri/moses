b78725305a8f1b0fb3f738f028c60b633eecaed1
Izel Nakri <contact@izelnakri.com>
2015-09-28 01:41:20 +0200

    first navigation logic

--- dev/js/moses.js
+++ dev/js/moses.js
@@ -10,10 +10,59 @@ $(document).ready(function() {
     });
   }
 
-  var filteredDom = $("#styleguide").children()
+  $("#styleguide").parent().append("<moses-navigation>
+    <moses-top-fixed-navigation>
+      <i class='fa fa-times'></i>
+      <h1>localhost:8080 Styleguide</h1>
+    </moses-top-fixed-navigation>
+
+    <moses-top-static-navigation>
+
+    </moses-top-static-navigation>
+
+    <moses-left-navigation>
+      <ul id='moses-foundation'>
+        <li><a href=''>Foundation</a></li>
+      </ul>
+      <ul id='moses-components'>
+        <li><a href=''>Components</a></li>
+      </ul>
+      <ul id='moses-templates'>
+        <li><a href=''>Templates</a></li>
+      </ul>
+    </moses-left-navigation>
+  </moses-navigation>");
+
+  $("moses-top-fixed-navigation").find(".fa").on("click", function(e) {
+    $(this).toggleClass("fa-bars").toggleClass("fa-times");
+    $("moses-left-navigation").toggle();
+    $("#styleguide").toggleClass("moses-left-navigation-inactive");
+    $("moses-top-fixed-navigation").toggleClass("moses-left-navigation-inactive");
+  })
+
+
+  var writtenScope = $("#styleguide").children()
+
+  var filteredDom = writtenScope
                 .not("h1").not("h2").not("p")
                 .not("script").not("style").not("link");
 
+
+  var idCount = 0;
+  for (i=1; i<=6; i++) {
+    if (i == 1) { var headersArray = [] }
+    // i = new Number(i);
+    var ref_array = writtenScope.filter("h" + i);
+    $.each(ref_array, function(i, e) {
+      idCount++;
+      $(e).attr('id', 'moses' + idCount);
+    });
+    headersArray = $.merge(headersArray, ref_array);
+  }(i)
+  
+  $.each(headersArray, function(i, e) { 
+    $("#moses-foundation").append("<a href='#" +$(e).attr('id') + "'>" + $(e).text() + "</a>"); 
+  });
 
   var getWrapper = function (html) {
     html = html.wrap("<div class='target'></div>");
@@ -36,29 +85,6 @@ $(document).ready(function() {
   // SyntaxHighlighter.defaults["html-script"] = true;
 
 
-  $("#styleguide").parent().append("<moses-navigation>
-    <moses-top-fixed-navigation>
-      <i class='fa fa-times'></i>
-      <h1>localhost:8080 Styleguide</h1>
-    </moses-top-fixed-navigation>
-
-    <moses-top-static-navigation>
-
-    </moses-top-static-navigation>
-
-    <moses-left-navigation>
-      <a href=''>Foundation</a>
-      <a href=''>Components</a>
-      <a href=''>Templets</a>
-    </moses-left-navigation>
-  </moses-navigation>");
-
-  $("moses-top-fixed-navigation").find(".fa").on("click", function(e) {
-    $(this).toggleClass("fa-bars").toggleClass("fa-times");
-    $("moses-left-navigation").toggle();
-    $("#styleguide").toggleClass("moses-left-navigation-inactive");
-    $("moses-top-fixed-navigation").toggleClass("moses-left-navigation-inactive");
-  })
 
 
 
