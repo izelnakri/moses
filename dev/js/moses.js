5f7f233096fdf42b90dff04aae7490d683d22293
Izel Nakri <contact@izelnakri.com>
2015-12-06 15:23:01 +0100

    update

--- dev/js/moses.js
+++ dev/js/moses.js
@@ -11,10 +11,13 @@ if (style.styleSheet){
 }
 
 head.appendChild(style);
+
 //also add font-awesome
-
-
-
+var fileref = document.createElement("link");
+fileref.setAttribute("rel", "stylesheet");
+fileref.setAttribute("type", "text/css");
+fileref.setAttribute("href", 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css');
+document.getElementsByTagName("head")[0].appendChild(fileref);
 
 $(document).ready(function() {
   // $("body").prepend()
